6293892a6e488397c663fcb99889655da8903d4a
veenabkatte <60433963+veenabkatte@users.noreply.github.com>
2021-09-20 23:43:05 +0530

    Decision tree program
    classes of its

---
+++ DecisionMakingConstructionDemo/DecisionMakingConstructionDemo/IfElseDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DecisionMakingConstructionDemo
+{
+    class IfElseDemo
+    {
+        static void Main()
+        {
+            Console.Write("Enter Annual Income:");
+            double annualIncome = Convert.ToDouble(Console.ReadLine());
+            if (annualIncome >= 280000)
+            {
+                Console.WriteLine("You are liable to pay I.T.");
+            }
+            else
+            {
+                Console.WriteLine("You are not liable to pay I.T.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DecisionMakingConstructionDemo/DecisionMakingConstructionDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DecisionMakingConstructionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Annual Income:");
+            double annualIncome = Convert.ToDouble(Console.ReadLine());
+            if (annualIncome >= 280000)
+            {
+                Console.WriteLine("You are liable to pay I.T.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
